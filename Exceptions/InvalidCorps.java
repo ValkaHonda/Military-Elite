5f57c4a802e2f9bb4af36496057875941dc14098
Valentin Aleksandrov <valentin805@gmail.com>
2017-10-22 15:19:30 +0300

    Create InvalidCorps.java

---
+++ Exceptions/InvalidCorps.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class InvalidCorps extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidCorps(String msg) {
+		super(msg);
+	}
+	
+}
